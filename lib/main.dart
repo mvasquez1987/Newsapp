def88998204de73a04fdd3006fb2ca7363061b1f
henry <mvasquez1987@hotmail.es>
2021-12-27 19:10:06 +0100

    Implemetando el boton navigator

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/src/pages/tabs_page.dart';
+import 'package:newsapp/src/theme/tema.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Material App',
+        theme: miTema,
+        debugShowCheckedModeBanner: false,
+        home: TabsPage());
+  }
+}
